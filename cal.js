59ff71456a311550494701a88cf70bbba2351650
kevemhenry32 <128760160+kevemhenry32@users.noreply.github.com>
2023-03-28 20:49:26 -0300

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,36 @@
+function calcular() {
+    console.log("passei")
+
+    let a = Number(document.getElementById("a").value);
+
+
+    let b = Number(document.getElementById("b").value);
+
+
+    let c = Number(document.getElementById("c").value);
+
+    let x = 0, x1 = 0, x2 = 0;
+
+
+    let delta = b ** 2 - 4 * a * c
+    console.log("o delta: " + delta);
+
+    if (delta == 0) {
+        alert("Delta é igual a " + delta)
+    } else {
+        alert("Delta é igual a " + delta)
+        x = Math.sqrt(delta)
+        console.log("x: " + x)
+        console.log("a raiz de Delta: " + x);
+        x1 = (-b + x) / (2 * a);
+
+        console.log("a raiz de x1: " + x1);
+        alert("O resultado de x1 é " + x1)
+        x2 = (-b - x) / (2 * a)
+
+        alert("O resultado de x2 é " + x2)
+        console.log("a raiz de x2: " + x2);
+    }
+
+
+}
